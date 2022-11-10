7ac1dd0765668e67e8bfcdfdedf484ce057a2e77
binary <binary.weng@idreamsky.com>
2022-11-10 17:19:03 +0800

    去掉 Loading 的 context 参数

--- lib/loading.dart
+++ lib/loading.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lib_theme/const.dart';
 import 'package:lib_utils/universal_platform.dart';
+import 'package:oktoast/oktoast.dart';
 
 import 'circular_progress.dart';
 import 'icon_font.dart';
@@ -16,7 +17,7 @@ enum LoadingActivityState {
 
 /// 显示loading效果
 class Loading {
-  static OverlayEntry? _entry;
+  static ToastFuture? toast;
 
   static Widget getActivityIndicator({
     double radius = 10.0,
@@ -63,65 +64,57 @@ class Loading {
   }
 
   /// 显示loading
-  static void show(
-    BuildContext context, {
-    bool isModal = true,
+  static void show({
     String? label,
     bool isEmpty = false,
   }) {
-    // assert(_entry == null, "不能出现两个 loading");
     hide();
-    final overlay = Overlay.of(context)!;
-    _entry = OverlayEntry(
-      builder: (_) {
-        return Material(
-          color: Colors.transparent,
-          child: isEmpty
-              ? const SizedBox()
-              : GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: isModal ? null : hide,
-                  child: Center(
-                    child: Container(
-                      width: 112,
-                      height: 112,
-                      decoration: BoxDecoration(
-                          color: Colors.black54,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            CircularProgress(
-                              primaryColor: Colors.white,
-                              secondaryColor: Colors.white.withOpacity(0),
-                              strokeWidth: 3,
-                              size: 33,
-                            ),
-                            if (label != null) sizeHeight24,
-                            if (label != null)
-                              Text(
-                                label,
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.white),
-                              ),
-                          ],
-                        ),
+    toast = showToastWidget(
+      Material(
+        color: Colors.transparent,
+        child: isEmpty
+            ? const SizedBox()
+            : Center(
+              child: Container(
+                width: 112,
+                height: 112,
+                decoration: BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgress(
+                        primaryColor: Colors.white,
+                        secondaryColor: Colors.white.withOpacity(0),
+                        strokeWidth: 3,
+                        size: 33,
                       ),
-                    ),
+                      if (label != null) sizeHeight24,
+                      if (label != null)
+                        Text(
+                          label,
+                          style: const TextStyle(
+                              fontSize: 14, color: Colors.white),
+                        ),
+                    ],
                   ),
                 ),
-        );
-      },
+              ),
+            ),
+      ),
+      animationDuration: Duration.zero,
+      duration: Duration.zero,
+      handleTouch: true
     );
-    overlay.insert(_entry!);
   }
 
   /// 关闭loading
   static void hide() {
-    if (_entry != null) {
-      _entry?.remove();
-      _entry = null;
+    if (toast != null) {
+      toast!.dismiss();
+      toast = null;
     }
   }
 
@@ -138,47 +131,41 @@ class Loading {
     hide();
     Timer(Duration(milliseconds: duration), hide);
 
-    final overlay = Overlay.of(context)!;
-    _entry = OverlayEntry(
-      builder: (_) {
-        return Material(
-          color: Colors.transparent,
-          child: isEmpty
-              ? const SizedBox()
-              : GestureDetector(
-                  behavior: HitTestBehavior.opaque,
-                  onTap: isModal ? null : hide,
+    toast = showToastWidget(Material(
+      color: Colors.transparent,
+      child: isEmpty
+          ? const SizedBox()
+          : GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: isModal ? null : hide,
+              child: Center(
+                child: Container(
+                  width: 128,
+                  height: 128,
+                  decoration: BoxDecoration(
+                      color: const Color(0xFF1A1A1A).withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(10)),
                   child: Center(
-                    child: Container(
-                      width: 128,
-                      height: 128,
-                      decoration: BoxDecoration(
-                          color: const Color(0xFF1A1A1A).withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            widget ?? getActivityState(state),
-                            if (label != null) sizeHeight24,
-                            if (label != null)
-                              Text(
-                                label,
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.white),
-                              ),
-                          ],
-                        ),
-                      ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        widget ?? getActivityState(state),
+                        if (label != null) sizeHeight24,
+                        if (label != null)
+                          Text(
+                            label,
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.white),
+                          ),
+                      ],
                     ),
                   ),
                 ),
-        );
-      },
-    );
-    overlay.insert(_entry!);
+              ),
+            ),
+    ));
   }
 
   /// 判断是否loading
-  static bool get visible => _entry != null;
+  static bool get visible => toast != null;
 }
