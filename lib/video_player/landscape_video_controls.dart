4261f88a772f75b9b600ca28e8aafc22590d67cc
binary <binary.weng@idreamsky.com>
2023-02-22 16:02:56 +0800

    横屏视频不允许侧滑返回

--- lib/video_player/landscape_video_controls.dart
+++ lib/video_player/landscape_video_controls.dart
@@ -96,200 +96,203 @@ class _LandscapeVideoControlsState extends State<LandscapeVideoControls>
   @override
   Widget build(BuildContext context) {
     final mq = MediaQuery.of(context);
-    return GestureDetector(
-      behavior: HitTestBehavior.translucent,
-      onTap: () {
-        setState(() {
-          _showControls = !_showControls;
-        });
-        if (_showControls) {
-          _startHideControlsCountdown();
-          _controlsFadeAnimation.forward();
-        } else {
-          _hideControlsTimer?.cancel();
-          _controlsFadeAnimation.reverse();
-        }
-      },
-      child: Stack(
-        fit: StackFit.expand,
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              height: 64 + mq.viewPadding.top,
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black.withOpacity(0.2),
-                    Colors.black.withOpacity(0)
-                  ],
-                ),
-              ))),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              height: 43 + mq.viewPadding.bottom,
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black.withOpacity(0),
-                    Colors.black.withOpacity(.2),
-                  ],
-                ),
-              ))),
-          // 屏幕中心的播放按钮
-          ValueListenableBuilder<bool>(
-              valueListenable: _isPlaying,
-              builder: (context, playing, child) =>
-                  Visibility(visible: !playing, child: child!),
-              child: UnconstrainedBox(
-                child: FadeButton(
-                  onTap: _togglePlay,
-                  child: const Icon(
-                    IconFont.play,
-                    size: 50,
-                    color: Colors.white,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          setState(() {
+            _showControls = !_showControls;
+          });
+          if (_showControls) {
+            _startHideControlsCountdown();
+            _controlsFadeAnimation.forward();
+          } else {
+            _hideControlsTimer?.cancel();
+            _controlsFadeAnimation.reverse();
+          }
+        },
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                height: 64 + mq.viewPadding.top,
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black.withOpacity(0.2),
+                      Colors.black.withOpacity(0)
+                    ],
                   ),
-                ),
-              )),
-
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // status bar and app bar
-              FadeTransition(
-                opacity: _opacityAnimation,
-                child: Column(
-                  children: [
-                    const SizedBox(height: 14),
-                    SizedBox(
-                      height: 44,
-                      child: Padding(
-                        padding: mq.viewPadding,
-                        child: Row(
-                          children: [
-                            FadeButton(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 10),
-                              child: SimpleShadow(
-                                sigma: 12,
-                                color: Colors.black,
-                                opacity: 0.1,
-                                child: const Icon(
-                                  IconFont.navBarBackItem,
-                                  size: 24,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              onTap: () {
-                                SystemChrome.setPreferredOrientations([
-                                  DeviceOrientation.portraitUp,
-                                  DeviceOrientation.portraitDown,
-                                ]);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
+                ))),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                height: 43 + mq.viewPadding.bottom,
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black.withOpacity(0),
+                      Colors.black.withOpacity(.2),
+                    ],
+                  ),
+                ))),
+            // 屏幕中心的播放按钮
+            ValueListenableBuilder<bool>(
+                valueListenable: _isPlaying,
+                builder: (context, playing, child) =>
+                    Visibility(visible: !playing, child: child!),
+                child: UnconstrainedBox(
+                  child: FadeButton(
+                    onTap: _togglePlay,
+                    child: const Icon(
+                      IconFont.play,
+                      size: 50,
+                      color: Colors.white,
                     ),
-                  ],
-                ),
-              ),
-              // status bar
+                  ),
+                )),
 
-              const Spacer(),
-              // 底部的控制栏
-              FadeTransition(
-                opacity: _opacityAnimation,
-                child: Padding(
-                  padding: mq.viewPadding,
-                  child: Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // status bar and app bar
+                FadeTransition(
+                  opacity: _opacityAnimation,
+                  child: Column(
                     children: [
-                      // 播放/暂停按钮
-                      ValueListenableBuilder<bool>(
-                          valueListenable: _isPlaying,
-                          builder: (context, isPlaying, child) {
-                            return FadeButton(
-                              onTap: _togglePlay,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(16, 12, 12, 12),
-                                child: Icon(
-                                  isPlaying ? IconFont.pause : IconFont.play,
-                                  color: Colors.white,
-                                  size: 24,
+                      const SizedBox(height: 14),
+                      SizedBox(
+                        height: 44,
+                        child: Padding(
+                          padding: mq.viewPadding,
+                          child: Row(
+                            children: [
+                              FadeButton(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 10),
+                                child: SimpleShadow(
+                                  sigma: 12,
+                                  color: Colors.black,
+                                  opacity: 0.1,
+                                  child: const Icon(
+                                    IconFont.navBarBackItem,
+                                    size: 24,
+                                    color: Colors.white,
+                                  ),
                                 ),
+                                onTap: () {
+                                  SystemChrome.setPreferredOrientations([
+                                    DeviceOrientation.portraitUp,
+                                    DeviceOrientation.portraitDown,
+                                  ]);
+                                },
                               ),
-                            );
-                          }),
-                      // 当前播放进度
-                      Container(
-                        width: 30 + 12,
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          _formatDuration(_videoProgress),
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      ),
-                      // 进度调整 Slider
-                      Expanded(
-                          child: SliderTheme(
-                        data: const SliderThemeData().copyWith(
-                          trackShape: RoundedVideoSliderTrackShape(),
-                          trackHeight: 2,
-                          thumbColor: const Color.fromRGBO(255, 255, 255, 1),
-                          thumbShape: const RoundSliderThumbShape(
-                            disabledThumbRadius: 4,
-                            enabledThumbRadius: 4,
-                            elevation: 0,
-                            pressedElevation: 0,
+                            ],
                           ),
-                          overlayColor: Colors.transparent,
-                          activeTrackColor: Colors.white.withOpacity(0.4),
-                          inactiveTrackColor: Colors.white.withOpacity(0.2),
-                        ),
-                        child: UnStyledVideoProgressSlider(
-                          widget.controller,
-                          onChangeStart: (_) {
-                            _hideControlsTimer?.cancel();
-                          },
-                          onChangeEnd: (v) {
-                            _startHideControlsCountdown();
-                            widget.eventTracker?.onSeek(v);
-                          },
-                        ),
-                      )),
-                      // 视频总时长
-                      Container(
-                        width: 30 + 12,
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          _formatDuration(widget.controller.value.duration),
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                // status bar
+
+                const Spacer(),
+                // 底部的控制栏
+                FadeTransition(
+                  opacity: _opacityAnimation,
+                  child: Padding(
+                    padding: mq.viewPadding,
+                    child: Row(
+                      children: [
+                        // 播放/暂停按钮
+                        ValueListenableBuilder<bool>(
+                            valueListenable: _isPlaying,
+                            builder: (context, isPlaying, child) {
+                              return FadeButton(
+                                onTap: _togglePlay,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(16, 12, 12, 12),
+                                  child: Icon(
+                                    isPlaying ? IconFont.pause : IconFont.play,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                ),
+                              );
+                            }),
+                        // 当前播放进度
+                        Container(
+                          width: 30 + 12,
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            _formatDuration(_videoProgress),
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        // 进度调整 Slider
+                        Expanded(
+                            child: SliderTheme(
+                          data: const SliderThemeData().copyWith(
+                            trackShape: RoundedVideoSliderTrackShape(),
+                            trackHeight: 2,
+                            thumbColor: const Color.fromRGBO(255, 255, 255, 1),
+                            thumbShape: const RoundSliderThumbShape(
+                              disabledThumbRadius: 4,
+                              enabledThumbRadius: 4,
+                              elevation: 0,
+                              pressedElevation: 0,
+                            ),
+                            overlayColor: Colors.transparent,
+                            activeTrackColor: Colors.white.withOpacity(0.4),
+                            inactiveTrackColor: Colors.white.withOpacity(0.2),
+                          ),
+                          child: UnStyledVideoProgressSlider(
+                            widget.controller,
+                            onChangeStart: (_) {
+                              _hideControlsTimer?.cancel();
+                            },
+                            onChangeEnd: (v) {
+                              _startHideControlsCountdown();
+                              widget.eventTracker?.onSeek(v);
+                            },
+                          ),
+                        )),
+                        // 视频总时长
+                        Container(
+                          width: 30 + 12,
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            _formatDuration(widget.controller.value.duration),
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
