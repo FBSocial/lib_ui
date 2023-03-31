1cdedc0aed63aeaf6d1a5f3f168816d0606c2d8b
chengguo <seven.cheng@idreamsky.com>
2023-03-31 20:21:13 +0800

    fix: 合并了最新的develop分支，解决了冲突

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,8 @@ import connectivity_plus_macos
 import device_info_plus_macos
 import devicelocale
 import package_info_plus_macos
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import wakelock_macos
