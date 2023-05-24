dc0cd8dee54f0e77832189899caa13b9fd0512a5
yuchang.xu <yuchang.xu@idreamsky.com>
2023-05-24 16:17:10 +0800

    fix: 恢复无效的文件变更

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,50 +7,26 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
-#include <dart_vlc/dart_vlc_plugin.h>
-#include <desktop_drop/desktop_drop_plugin.h>
-#include <desktop_lifecycle/desktop_lifecycle_plugin.h>
-#include <desktop_open_file/desktop_open_file_plugin.h>
-#include <fc_native_video_thumbnail/fc_native_video_thumbnail_plugin_c_api.h>
 #include <flutter_image_view/flutter_image_view_plugin.h>
-#include <flutter_native_view/flutter_native_view_plugin.h>
 #include <pasteboard/pasteboard_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
-#include <screen_retriever/screen_retriever_plugin.h>
+#include <screen_capture_assistant/screen_capture_assistant_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <win_toast/win_toast_plugin.h>
-#include <window_manager/window_manager_plugin.h>
 #include <zego_express_engine/zego_express_engine_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  DartVlcPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DartVlcPlugin"));
-  DesktopDropPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DesktopDropPlugin"));
-  DesktopLifecyclePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DesktopLifecyclePlugin"));
-  DesktopOpenFilePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("DesktopOpenFilePlugin"));
-  FcNativeVideoThumbnailPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FcNativeVideoThumbnailPluginCApi"));
   FlutterImageViewPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterImageViewPlugin"));
-  FlutterNativeViewPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterNativeViewPlugin"));
   PasteboardPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PasteboardPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
-  ScreenRetrieverPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
+  ScreenCaptureAssistantPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenCaptureAssistantPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  WinToastPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WinToastPlugin"));
-  WindowManagerPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
   ZegoExpressEnginePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ZegoExpressEnginePlugin"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -4,19 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus
-  dart_vlc
-  desktop_drop
-  desktop_lifecycle
-  desktop_open_file
-  fc_native_video_thumbnail
   flutter_image_view
-  flutter_native_view
   pasteboard
   permission_handler_windows
-  screen_retriever
+  screen_capture_assistant
   url_launcher_windows
-  win_toast
-  window_manager
   zego_express_engine
 )
 
