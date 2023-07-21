5c7f1d823a5337202f511251947219debed55091
qubian <qubian@idreamsky.com>
2023-07-21 14:30:31 +0800

    fix: 格式化

--- lib/fanbook_icon.dart
+++ lib/fanbook_icon.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lib_theme/app_theme.dart';
 
 class FanbookIcon extends StatelessWidget {
   final double size;
