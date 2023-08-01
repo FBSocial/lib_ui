b8f9bef5d78e3e640cf65abbb314490c5b273079
johnson.zhong <514961926@qq.com>
2023-08-01 16:44:42 +0800

    fix: 修改编译错误

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,32 +7,44 @@ import Foundation
 
 import audioplayers
 import connectivity_plus
+import dart_vlc
+import desktop_drop
+import desktop_lifecycle
+import desktop_open_file
 import device_info_plus_macos
 import devicelocale
-import file_selector_macos
-import package_info_plus_macos
+import fc_native_video_thumbnail
+import flutter_local_notifications
+import flutter_parfait
+import package_info_plus
 import pasteboard
 import path_provider_foundation
-import screen_capture_assistant
+import screen_retriever
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 import wakelock_macos
-import zego_express_engine
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
+  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
+  DesktopLifecyclePlugin.register(with: registry.registrar(forPlugin: "DesktopLifecyclePlugin"))
+  DesktopOpenFilePlugin.register(with: registry.registrar(forPlugin: "DesktopOpenFilePlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   DevicelocalePlugin.register(with: registry.registrar(forPlugin: "DevicelocalePlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FcNativeVideoThumbnailPlugin.register(with: registry.registrar(forPlugin: "FcNativeVideoThumbnailPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterParfaitPlugin.register(with: registry.registrar(forPlugin: "FlutterParfaitPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenCaptureAssistantPlugin.register(with: registry.registrar(forPlugin: "ScreenCaptureAssistantPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
-  ZegoExpressEnginePlugin.register(with: registry.registrar(forPlugin: "ZegoExpressEnginePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,29 +7,47 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus/connectivity_plus_windows_plugin.h>
-#include <file_selector_windows/file_selector_windows.h>
+#include <dart_vlc/dart_vlc_plugin.h>
+#include <desktop_drop/desktop_drop_plugin.h>
+#include <desktop_lifecycle/desktop_lifecycle_plugin.h>
+#include <desktop_open_file/desktop_open_file_plugin.h>
+#include <fc_native_video_thumbnail/fc_native_video_thumbnail_plugin_c_api.h>
 #include <flutter_image_view/flutter_image_view_plugin.h>
+#include <flutter_native_view/flutter_native_view_plugin.h>
 #include <pasteboard/pasteboard_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
-#include <screen_capture_assistant/screen_capture_assistant_plugin_c_api.h>
+#include <screen_retriever/screen_retriever_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <zego_express_engine/zego_express_engine_plugin.h>
+#include <win_toast/win_toast_plugin.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  FileSelectorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  DartVlcPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DartVlcPlugin"));
+  DesktopDropPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopDropPlugin"));
+  DesktopLifecyclePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopLifecyclePlugin"));
+  DesktopOpenFilePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopOpenFilePlugin"));
+  FcNativeVideoThumbnailPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FcNativeVideoThumbnailPluginCApi"));
   FlutterImageViewPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterImageViewPlugin"));
+  FlutterNativeViewPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterNativeViewPlugin"));
   PasteboardPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PasteboardPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
-  ScreenCaptureAssistantPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ScreenCaptureAssistantPluginCApi"));
+  ScreenRetrieverPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenRetrieverPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  ZegoExpressEnginePluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ZegoExpressEnginePlugin"));
+  WinToastPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WinToastPlugin"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
