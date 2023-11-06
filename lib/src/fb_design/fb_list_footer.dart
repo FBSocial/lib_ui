386587ccd691b2843c67dbc61c3c04d619a975d0
jinping.shi <jinping.shi@idreamsky.com>
2023-11-07 07:22:30 +0800

    feat: 清理历史部分todo

--- lib/src/fb_design/fb_list_footer.dart
+++ lib/src/fb_design/fb_list_footer.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:lib_ui/load_more.dart';
+
 enum FbLoadStatus {
   idle,
   loading,
   error,
   noMore,
 }
+
 /// 为列表提供加载态和重试机制
 class FbListFooter extends StatelessWidget {
   final Widget child;
@@ -27,21 +29,17 @@ class FbListFooter extends StatelessWidget {
       onNotification: (notification) {
         final metrics = notification.metrics;
         if (metrics.pixels >= metrics.maxScrollExtent) {
-          switch(loadStatus) {
-
+          // TODO: Handle cases.
+          switch (loadStatus) {
             case LoadMoreStatus.noMore:
               break;
             case LoadMoreStatus.ready:
-              // TODO: Handle this case.
               break;
             case LoadMoreStatus.loading:
-              // TODO: Handle this case.
               break;
             case LoadMoreStatus.error:
-              // TODO: Handle this case.
               break;
             case LoadMoreStatus.toTop:
-              // TODO: Handle this case.
               break;
           }
         }
